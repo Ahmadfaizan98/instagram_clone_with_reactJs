f361cc39add3b7a8ee932d0615d0afa18f36b3ab
Ahmad Faizan <ahmad.faizan@fortray.com>
2026-09-24 16:10:19 +0100

    login-page-css

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -12,20 +12,51 @@ function LoginPage() {
             <div className="container">
                 <Grid container>
                     <Grid className="insta_login_left_content" size={{ xs: 12, md: 8 }}>
-                        <img className="insta-logo" src={instaLogo} alt="Instagram" width={72} height={72} />
+                        <img className="insta-logo" src={instaLogo} alt="Instagram" width={60} height={60} />
 
                         <div className="flex-row">
                             <h1>
-                                See everyday moments from your close friends.
+                                See everyday moments from your <span className="gradient-text"> close friends</span>.
                             </h1>
 
-                            <img className="insta-login-img" src={instaLogin} alt="Instagram login" width={1240} height={1025} />
+                            <img className="insta-login-img" src={instaLogin} alt="Instagram login" width={700} height={500} />
                         </div>
                     </Grid>
 
                     <Grid className="insta_login_right_content" size={{ xs: 12, md: 4 }}>
                         <div className="login-content">
-                            Faizan
+                            <div className="login-box">
+                                <h2>Log into Instagram</h2>
+
+                                <form className="login-form">
+                                    <input type="text" placeholder="Mobile number, username or email" />
+
+                                    <input type="password" placeholder="Password" />
+
+                                    <button type="submit" className="login-button">Log in</button>
+                                </form>
+
+                                <button type="button" className="forgot-password">Forgot password?</button>
+
+                                <div className="login-divider">
+                                    <span></span>
+                                    <span>OR</span>
+                                    <span></span>
+                                </div>
+
+                                <button type="button" className="facebook-login">
+                                    <span className="facebook-icon">f</span> Log in with Facebook
+                                </button>
+
+                                <button type="button" className="create-account">
+                                    Create new account
+                                </button>
+
+                                <div className="meta-brand">
+                                    <span className="meta-logo">∞</span>
+                                    <span>Meta</span>
+                                </div>
+                            </div>
                         </div>
                     </Grid>
                 </Grid>
